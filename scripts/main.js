a84ce1415459bacbb48197c26251a2b3d1de2ebe
JefVH <BEJHW@LABODEV12.deceuninck.com>
2015-04-22 10:11:59 +0200

    bedrijfsbezoek fields

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,54 @@
-﻿
+﻿$(function () {
+    $('#timepicker').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime:'12:00'
+    });
+    $('#timepicker2').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $('#timepicker3').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $('#timepicker4').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $('#timepicker5').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $('#timepicker6').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $('#timepicker7').timepicker({
+        minuteStep: 15,
+        template: 'dropdown',
+        showMeridian: false,
+        showSeconds: false,
+        defaultTime: '12:00'
+    });
+    $(document).on('click', '.dropdown-menu.dropdown-menu-form', function (e) {
+        e.stopPropagation();
+    });
+});
